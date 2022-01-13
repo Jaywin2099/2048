f449f1db2029b2e6133b81cefe258b119e5999fb
Jacob Winter <jaywin2099@gmail.com>
2022-01-13 17:34:22 -0600

    displays pink grid now

--- bin/2048.js
+++ bin/2048.js
@@ -1,8 +1,9 @@
-'./game.js';
-
 //configuration variables
-const cellpadding = 5;
-const tileRadius = (canvas.width / 4 - cellpadding * 3) / 2; // maybe subract some padding for the canvas width if you cant do that in css
+const cellpadding = 8;
+let tileRadius;
+if (canvas.width > canvas.height) tileRadius = (canvas.height / 4 - cellpadding) / 2; 
+else tileRadius = (canvas.width / 4 - cellpadding * 3) / 2; 
+// maybe subract some padding for the canvas width if you cant do that in css
 
 const emptyGrid = [
     [null, null, null, null],
@@ -29,8 +30,13 @@ const game = () => {
     c.fillStyle = 'white';
     c.fillRect(0, 0, canvas.width, canvas.height);
 
-    addRandomTile();
-    console.log(grid);
+    //draw tile fillers
+    c.fillStyle = 'pink';
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            c.fillRect(i * (cellpadding + tileRadius * 2), j * (cellpadding + tileRadius * 2), tileRadius * 2, tileRadius * 2);
+        }
+    }
 }
 
 //initiate game interval
